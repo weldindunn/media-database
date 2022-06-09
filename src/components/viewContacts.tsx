85202335e8672458e8c0255e9eec8e2ce09186fa
Dunn <wdunn@delaware.coop>
2022-06-09 15:22:47 -0400

    Can add new contacts

--- src/components/viewContacts.tsx
+++ src/components/viewContacts.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Table } from "react-bootstrap";
+import { Form, Table, Button } from "react-bootstrap";
 import { Contact } from "../interfaces/contact";
 import contacts from "../data/contacts.json";
 
@@ -7,6 +7,44 @@ const CONTACTS = contacts.map((contact): Contact => ({...contact}));
 
 export function ViewContacts(): JSX.Element {
     const [contacts, setContacts] = useState<Contact[]>(CONTACTS);
+
+    const [industry, setIndustry] = useState<string>("");
+    const [organization, setOrganization] = useState<string>("");
+    const [department, setDepartment] = useState<string>("");
+    const [name, setName] = useState<string>("");
+    const [phoneNumber, setPhoneNumber] = useState<string>("");
+    const [email, setEmail] = useState<string>("");
+    const [city, setCity] = useState<string>("");
+    const [state, setState] = useState<string>("");
+
+    function save(): void {
+        setContacts(
+            [...contacts, {
+                id: contacts.length + 1,
+                industry: industry,
+                organization: organization,
+                department: department,
+                name: name,
+                phone_number: phoneNumber,
+                email: email,
+                address: "",
+                city: city,
+                state: state
+            }]
+        )
+        reset();
+    }
+
+    function reset(): void {
+        setIndustry("");
+        setOrganization("");
+        setDepartment("");
+        setName("");
+        setPhoneNumber("");
+        setEmail("");
+        setCity("");
+        setState("");
+    }
 
     return (
         <div className="contactTable">
@@ -21,6 +59,7 @@ export function ViewContacts(): JSX.Element {
                         <th>Email</th>
                         <th>City</th>
                         <th>State</th>
+                        <th>Edit</th>
                         {/*<th>Address</th>*/}
                     </tr>
                 </thead>
@@ -35,10 +74,83 @@ export function ViewContacts(): JSX.Element {
                             <td>{contact.email}</td>
                             <td>{contact.city}</td>
                             <td>{contact.state}</td>
+                            <td>
+                                <Button>Edit</Button>
+                            </td>
                             {/*<td>{contact.address}{", "}{contact.city}{", "}{contact.state}</td>*/}
                         </tr>
                         )
                     )}
+                    <tr>
+                        {/*Industry*/}
+                        <td>
+                            <Form.Control
+                                value={industry}
+                                onChange={(event: React.ChangeEvent<HTMLInputElement>) => setIndustry(event.target.value)} 
+                                placeholder="Enter Industry"
+                            />
+                        </td>
+                        {/*Organization*/}
+                        <td>
+                            <Form.Control
+                                value={organization}
+                                onChange={(event: React.ChangeEvent<HTMLInputElement>) => setOrganization(event.target.value)} 
+                                placeholder="Enter Organization"
+                            />
+                        </td>
+                        {/*Department*/}
+                        <td>
+                            <Form.Control
+                                value={department}
+                                onChange={(event: React.ChangeEvent<HTMLInputElement>) => setDepartment(event.target.value)} 
+                                placeholder="Enter Department"
+                            />
+                        </td>
+                        {/*Name*/}
+                        <td>
+                            <Form.Control
+                                value={name}
+                                onChange={(event: React.ChangeEvent<HTMLInputElement>) => setName(event.target.value)} 
+                                placeholder="Enter Name"
+                            />
+                        </td>
+                        {/*Phone Number*/}
+                        <td>
+                            <Form.Control
+                                value={phoneNumber}
+                                onChange={(event: React.ChangeEvent<HTMLInputElement>) => setPhoneNumber(event.target.value)} 
+                                placeholder="Enter Phone Number"
+                            />
+                        </td>
+                        {/*Email*/}
+                        <td>
+                            <Form.Control
+                                value={email}
+                                onChange={(event: React.ChangeEvent<HTMLInputElement>) => setEmail(event.target.value)} 
+                                placeholder="Enter Email"
+                            />
+                        </td>
+                        {/*City*/}
+                        <td>
+                            <Form.Control
+                                value={city}
+                                onChange={(event: React.ChangeEvent<HTMLInputElement>) => setCity(event.target.value)} 
+                                placeholder="Enter City"
+                            />
+                        </td>
+                        {/*State*/}
+                        <td>
+                            <Form.Control
+                                value={state}
+                                onChange={(event: React.ChangeEvent<HTMLInputElement>) => setState(event.target.value)} 
+                                placeholder="Enter State"
+                            />
+                        </td>
+                        {/*Save*/}
+                        <td>
+                            <Button onClick={save}>Save</Button>
+                        </td>
+                    </tr>
                 </tbody>
             </Table>
         </div>
