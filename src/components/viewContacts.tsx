ac442dabb68f64c5fef35136537dc8c8869ff9a2
Dunn <wdunn@delaware.coop>
2022-06-09 11:52:57 -0400

    Added example data

--- src/components/viewContacts.tsx
+++ src/components/viewContacts.tsx
@@ -1,9 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
+import { Table } from "react-bootstrap";
+import { Contact } from "../interfaces/contact";
+import contacts from "../data/contacts.json";
+
+const CONTACTS = contacts.map((contact): Contact => ({...contact}));
 
 export function ViewContacts(): JSX.Element {
+    const [contacts, setContacts] = useState<Contact[]>(CONTACTS);
+
     return (
         <div>
-            <span>This is where I'm gonna put all the contact info</span>
+            <Table striped bordered hover>
+                <thead>
+                    <tr>
+                        <th>Industry</th>
+                        <th>Organization</th>
+                        <th>Department</th>
+                        <th>Name</th>
+                        <th>Phone Number</th>
+                        <th>Email</th>
+                        <th>City</th>
+                        <th>State</th>
+                        <th>Address</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {contacts.map((contact: Contact) => (
+                        <tr key={contact.id}>
+                            <td>{contact.industry}</td>
+                            <td>{contact.organization}</td>
+                            <td>{contact.department}</td>
+                            <td>{contact.name}</td>
+                            <td>{contact.phone_number}</td>
+                            <td>{contact.email}</td>
+                            <td>{contact.city}</td>
+                            <td>{contact.state}</td>
+                            <td>{contact.address}{", "}{contact.city}{", "}{contact.state}</td>
+                        </tr>
+                        )
+                    )}
+                </tbody>
+            </Table>
         </div>
     )
 }
---
+++ src/interfaces/contact.ts
@@ -0,0 +1,12 @@
+export interface Contact {
+    id: number;
+    name: string;
+    department: string;
+    organization: string;
+    industry: string;
+    phone_number: string;
+    email: string;
+    address: string;
+    city: string;
+    state: string;
+}
