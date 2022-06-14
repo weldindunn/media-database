7b0664a602ad555d1a74e27f5af878b715f3fb6a
Dunn <wdunn@delaware.coop>
2022-06-14 13:16:57 -0400

    Created Legislation table and edit modal

---
+++ src/components/editLegislation.tsx
@@ -0,0 +1,173 @@
+import React, { useState } from "react";
+import { Form, Modal, Col, Row, Button } from "react-bootstrap";
+import  { Legislation } from "../interfaces/legislation";
+
+export function EditLegislation({
+    leg,
+    legislation,
+    setLegislation
+
+}: {
+    leg: Legislation;
+    legislation: Legislation[];
+    setLegislation: (legislation: Legislation[]) => void;
+}): JSX.Element {
+
+    const [title, setTitle] = useState<string>(leg.title);
+    const [status, setStatus] = useState<string>(leg.status);
+    const [sponsors, setSponsors] = useState<string>(leg.sponsors.toString());
+    const [summary, setSummary] = useState<string>(leg.summary);
+    const [source, setSource] = useState<string>(leg.source);
+
+    const [isEditing, changeEditing] = useState<boolean>(false);
+
+    function openModal(): void {
+        changeEditing(true);
+    }
+
+    function closeModal(): void {
+        changeEditing(false);
+    }
+
+    function save(): void {
+        const d = new Date();
+        //(d.getMonth() + 1).toString() + "/" + d.getDate().toString() + "/" + d.getFullYear().toString().substring(2)
+
+        setLegislation(legislation.map((targetLeg: Legislation): Legislation => targetLeg.id === leg.id ? 
+                ({
+                    id: leg.id,
+                    title: title,
+                    status: status,
+                    lastUpdated: (d.getMonth() + 1).toString() + "/" + d.getDate().toString() + "/" + d.getFullYear().toString().substring(2),
+                    sponsors: sponsors.split(", "),
+                    summary: summary,
+                    source: source
+                })
+                : (targetLeg)
+            )
+        );
+        closeModal();
+    }
+
+    function cancel(): void {
+        setTitle(leg.title);
+        setStatus(leg.status);
+        setSponsors(leg.sponsors.toString());
+        setSummary(leg.summary);
+        setSource(leg.source);
+        closeModal();
+    }
+
+    /*
+    function reset(): void {
+        setIndustry("");
+        setOrganization("");
+        setDepartment("");
+        setName("");
+        setPhoneNumber("");
+        setEmail("");
+        setCity("");
+        setState("");
+        closeModal();
+    }
+    */
+
+    return (
+        <div>
+            <Button onClick={openModal}>Edit</Button>
+            <Modal
+                show={isEditing}
+                onHide={closeModal}
+                animation={true}
+            >
+                <Modal.Header>
+                    <Modal.Title>Edit Legislation</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    {/* Title */}
+                    <Form.Group controlId="formLegTitle" as={Row}>
+                        <Form.Label column sm={3}>
+                            Title
+                        </Form.Label>
+                        <Col>
+                            <Form.Control
+                                value={title}
+                                onChange={(
+                                    event: React.ChangeEvent<HTMLInputElement>
+                                    ) => setTitle(event.target.value)}
+                                placeholder={"Enter Title"}
+                            />
+                        </Col>
+                    </Form.Group>
+                    {/* Status */}
+                    <Form.Group controlId="formLegStatus" as={Row}>
+                        <Form.Label column sm={3}>
+                            Status
+                        </Form.Label>
+                        <Col>
+                            <Form.Control
+                                value={status}
+                                onChange={(
+                                    event: React.ChangeEvent<HTMLInputElement>
+                                    ) => setStatus(event.target.value)}
+                                placeholder={"Enter Status"}
+                            />
+                        </Col>
+                    </Form.Group>
+                    {/* Sponsors */}
+                    <Form.Group controlId="formLegSponsors" as={Row}>
+                        <Form.Label column sm={3}>
+                            Sponsors
+                        </Form.Label>
+                        <Col>
+                            <Form.Control
+                                value={sponsors}
+                                onChange={(
+                                    event: React.ChangeEvent<HTMLInputElement>
+                                    ) => setSponsors(event.target.value)}
+                                placeholder={"Enter Sponsors"}
+                            />
+                        </Col>
+                    </Form.Group>
+                    {/* Summary */}
+                    <Form.Group controlId="formLegSummary" as={Row}>
+                        <Form.Label column sm={3}>
+                            Summary
+                        </Form.Label>
+                        <Col>
+                            <Form.Control
+                                value={summary}
+                                onChange={(
+                                    event: React.ChangeEvent<HTMLInputElement>
+                                    ) => setSummary(event.target.value)}
+                                placeholder={"Enter Summary"}
+                            />
+                        </Col>
+                    </Form.Group>
+                    {/* Source */}
+                    <Form.Group controlId="formLegSource" as={Row}>
+                        <Form.Label column sm={3}>
+                            Source
+                        </Form.Label>
+                        <Col>
+                            <Form.Control
+                                value={source}
+                                onChange={(
+                                    event: React.ChangeEvent<HTMLInputElement>
+                                    ) => setSource(event.target.value)}
+                                placeholder={"Enter Source"}
+                            />
+                        </Col>
+                    </Form.Group>
+                </Modal.Body>
+                <Modal.Footer>
+                    {/* Save Button */}
+                    <Button onClick={save} variant="success">Save</Button>
+
+                    {/* Cancel Button */}
+                    <Button onClick={cancel} variant="warning">Cancel</Button>
+                </Modal.Footer>
+            </Modal>
+        </div>
+    )
+}
---
+++ src/components/viewLegislation.tsx
@@ -0,0 +1,150 @@
+import React, { useState } from "react";
+import { Form, Table, Button, Dropdown } from "react-bootstrap";
+import { Legislation } from "../interfaces/legislation";
+import { EditLegislation } from "./editLegislation";
+import legislation from "../data/legislation.json";
+
+const LEGISLATION = legislation.map((legislation): Legislation => ({...legislation}));
+
+export function ViewLegislation(): JSX.Element {
+    const [legislation, setLegislation] = useState<Legislation[]>(LEGISLATION);
+
+    const [title, setTitle] = useState<string>("");
+    const [status, setStatus] = useState<string>("");
+    const [sponsors, setSponsors] = useState<string>("");
+    const [summary, setSummary] = useState<string>("");
+    const [source, setSource] = useState<string>("");
+
+    function save(): void {
+        const d = new Date();
+
+        setLegislation(
+            [...legislation, {
+                id: legislation.length + 1,
+                title: title,
+                status: status,
+                lastUpdated: (d.getMonth() + 1).toString() + "/" + d.getDate().toString() + "/" + d.getFullYear().toString().substring(2),
+                sponsors: sponsors.split(", "),
+                summary: summary,
+                source: source
+            }]
+        );
+        reset();
+    }
+
+    function reset(): void {
+        setTitle("");
+        setStatus("");
+        setSponsors("");
+        setSummary("");
+        setSource("");
+    }
+
+    return (
+        <div className="legislationTable">
+            <Table striped bordered hover responsive="sm">
+                <thead>
+                    <tr>
+                        <th>Title</th>
+                        <th>Status</th>
+                        <th>Last Updated</th>
+                        <th>Sponsors</th>
+                        <th>Summary</th>
+                        <th>Source</th>
+                        <th>Edit</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {legislation.map((leg: Legislation) => (
+                        <tr key={leg.id}>
+                            <td>{leg.title}</td>
+                            <td>{leg.status}</td>
+                            <td>{leg.lastUpdated}</td>
+                            <td>
+                                <Dropdown>
+                                    <Dropdown.Toggle variant="outline-dark" id="dropdown-basic">
+                                        Sponsors
+                                    </Dropdown.Toggle>
+
+                                    <Dropdown.Menu>
+                                        {
+                                            leg.sponsors.map((sponsor: string) => (
+                                                <Dropdown.ItemText>
+                                                    {sponsor}
+                                                </Dropdown.ItemText>
+                                            ))
+                                        }
+                                    </Dropdown.Menu>
+                                </Dropdown>
+                            </td>
+                            <td>{leg.summary}</td>
+                            <td><a href={leg.source} target="_blank" rel="noreferrer">Source</a></td>
+                            <td>
+                                <EditLegislation
+                                    leg={leg}
+                                    legislation={legislation}
+                                    setLegislation={setLegislation}
+                                ></EditLegislation>
+                            </td>
+                        </tr>
+                        )
+                    )}
+                    <tr key="LegislationInput">
+                        {/*Title*/}
+                        <td>
+                            <Form.Control
+                                value={title}
+                                onChange={(event: React.ChangeEvent<HTMLInputElement>) => setTitle(event.target.value)} 
+                                placeholder="Enter Title"
+                            />
+                        </td>
+                        {/*Status*/}
+                        <td>
+                            <Form.Control
+                                value={status}
+                                onChange={(event: React.ChangeEvent<HTMLInputElement>) => setStatus(event.target.value)} 
+                                placeholder="Enter Status"
+                            />
+                        </td>
+                        {/*Date*/}
+                        <td>
+                            <Form.Control
+                                onChange={(event: React.ChangeEvent<HTMLInputElement>) => setStatus(event.target.value)} 
+                                placeholder={Date()}
+                                disabled
+                            />
+                        </td>
+                        {/*Sponsors*/}
+                        <td>
+                            <Form.Control
+                                value={sponsors}
+                                onChange={(event: React.ChangeEvent<HTMLInputElement>) => setSponsors(event.target.value)} 
+                                placeholder="Enter Sponsors"
+                            />
+                        </td>
+                        {/*Summary*/}
+                        <td>
+                            <Form.Control
+                                value={summary}
+                                onChange={(event: React.ChangeEvent<HTMLInputElement>) => setSummary(event.target.value)} 
+                                placeholder="Enter Summary"
+                            />
+                        </td>
+                        {/*Source*/}
+                        <td>
+                            <Form.Control
+                                value={source}
+                                onChange={(event: React.ChangeEvent<HTMLInputElement>) => setSource(event.target.value)} 
+                                placeholder="Enter URL"
+                            />
+                        </td>
+                        {/*Save*/}
+                        <td>
+                            <Button onClick={save}>Save</Button>
+                        </td>
+                    </tr>
+                </tbody>
+            </Table>
+        </div>
+    )
+}
